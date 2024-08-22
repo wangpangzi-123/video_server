4d60228ef7dc8246ae923c7caa012725f94bee3c
wangpangzi-123 <1097753194@qq.com>
2024-08-22 22:00:44 +0800

    leave process_communicate //https://zhuanlan.zhihu.com/p/605960586?ssr_src=heifetz

--- project/function.h
+++ project/function.h
@@ -1,10 +1,17 @@
 #pragma once
 #include <functional>
 #include <optional>
+#include <sys/socket.h>
+
+
+
+
 class process_helper
 {
 public:
 	process_helper() {}
+
+	virtual ~process_helper() {}
 
 	template<typename function, typename... Args>
 	process_helper(function&& func, Args&&... args)
@@ -38,9 +45,11 @@ public:
 			return -1;
 		pid_t pid = fork();
 		if (pid == -1)
-			return -2;
-		if (pid == 0)
+			return -2;	//子进程创建失败
+
+		if (pid == 0)	
 		{
+			//子进程
 			printf("m_pid = %d\r\n", pid);
 			return run_bind_func();
 		}
@@ -50,6 +59,43 @@ public:
 		return 0;
 	}
 
+	//测试-> 
+	int send_fd(int& fd)
+	{
+		//
+		struct msghdr	msg;
+		struct cmsghdr* p_cmsg;
+		struct iovec	vec;
+
+		
+		//假设传送的数据 是 int 类型数组，模拟传送多个 文件描述符
+		int fd_array[5] = { 1, 2, 3, 4, 5 };
+		
+		//1、填充 msg
+		msg.msg_name = NULL;
+		msg.msg_namelen = 0;
+		msg.msg_iov = &vec;
+		msg.msg_iovlen = 1;
+		msg.msg_flags = 0;
+
+		
+		//2、至少附带 1 bytes 的真实数据
+		char real_data = 'a';
+		vec.iov_base = &real_data;
+		vec.iov_len = sizeof(real_data);
+
+
+		//3、填充附属数据，将文件描述符填入 附属数据
+		p_cmsg = CMSG_FIRSTHDR(&msg);	// 附属数据指针
+		p_cmsg->cmsg_level = SOL_SOCKET;
+		p_cmsg->cmsg_type = SCM_RIGHTS;
+		p_cmsg->cmsg_len = CMSG_LEN(sizeof(fd_array));
+		int* p_fds = (int*)CMSG_DATA(p_cmsg);
+		p_fds = fd_array;
+		
+	
+		//
+	}
 
 private:
 	std::function<int(void)> m_binder;
--- project/main.cpp
+++ project/main.cpp
@@ -12,6 +12,7 @@ int hello(int a)
     return 0;
 }
 
+#include<sys/socket.h>
 int main() {
     // 创建一个进程,传入一个 lambda 函数和参数
     process_helper process;
@@ -20,7 +21,13 @@ int main() {
     process.create_sub_process();
     
 
+    char buf[2][10] = { "a", "b" };
+    std::cout << sizeof(buf[0]) << std::endl;;
+
+    int fd_array[5] = { 1, 2, 3, 4, 5 };
+    std::cout << "fd_array : " << sizeof(fd_array) << std::endl;
     
+    int a = 0;
 
 
     return 0;
